d336fad9d53ad4cd4556b76a3e047870da2335de
axelolea <axelcedilloolea@gmail.com>
2023-06-05 23:47:49 -0600

    add settings in run and part exception

--- app/parts/router.py
+++ app/parts/router.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import APIRouter, status, Body, Path
+from fastapi import APIRouter, status, Body, Path, HTTPException
 from app.parts.schemas import ListPartsSchema, PartSchema
 from app.parts.crud import PartRepositoryDep
 
@@ -7,7 +7,7 @@ router = APIRouter()
 
 
 @router.get(
-    '/',
+    '',
     status_code=status.HTTP_200_OK,
     response_model=ListPartsSchema
 )
@@ -23,16 +23,20 @@ async def get_parts(
     status_code=status.HTTP_200_OK,
     response_model=PartSchema
 )
-async def get_part(
+async def get_part_with_number(
         number: Annotated[int, Path()],
         part_repository: PartRepositoryDep
 ):
     part = part_repository.find_one(number)
+    if not part:
+        raise HTTPException(
+            status_code=404,
+        )
     return part
 
 
 @router.post(
-    '/',
+    '',
     status_code=status.HTTP_201_CREATED,
     response_model=PartSchema
 )
--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import uvicorn
+from app.core.config import get_settings
+
+settings = get_settings()
 
 if __name__ == '__main__':
     uvicorn.run(
         'app.main:app',
-        reload=True,
-        port=8080
+        reload=settings.reload,
+        port=settings.port
     )
